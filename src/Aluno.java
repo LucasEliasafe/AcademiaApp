8b9d9ec98f0516658b184ffa1bd6d548933ebe41
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-18 20:38:45 -0300

    Iniciando projeto de app para academia com Jframe

---
+++ src/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+    private String nome;
+    private int idade;
+    private String[] aparelhos;
+
+    public Aluno(String nome, int idade, String[] aparelhos) {
+        this.nome = nome;
+        this.idade = idade;
+        this.aparelhos = aparelhos;
+    }
+
+
+}
