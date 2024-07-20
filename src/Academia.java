2233d7eb8a662c61409b9fe0adf2efe8b098ee46
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-20 09:11:30 -0300

    Feat: Adicionar métodos para adicionar e remover da lista da academia

--- src/Academia.java
+++ src/Academia.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Academia {
-    private List<Aluno> alunos;
-    private List<Aparelho> aparelhos;
+    public class Academia {
+        private List<Aluno> alunos;
+        private List<Aparelho> aparelhos;
 
     public Academia() {
-
+        this.alunos = new ArrayList<>();
+        this.aparelhos = new ArrayList<>();
     }
+
+    public void adicionarAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+
 }
