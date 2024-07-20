866617bcfa57befeada104a99d4dba0d6b0083f3
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-20 09:00:04 -0300

    Feat:Criar os métodos para pegar o nome e a descrição dos aparelhos na classe aparelhos

--- src/Aparelho.java
+++ src/Aparelho.java
@@ -1,2 +1,11 @@
 public class Aparelho {
+    private String nome;
+    private String descricao;
+
+    public Aparelho(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+
 }
