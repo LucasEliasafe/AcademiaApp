a4cce87c799c0dd2b71f007dd8d5205d4b0097af
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-20 09:54:22 -0300

    Feat: adicionar métodos para adicionar painéis com os botões de informações do Aluno e Aparelhos

--- src/AcademiaGUI.java
+++ src/AcademiaGUI.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-    public class AcademiaGUI {
+    public class AcademiaGUI extends JFrame {
         private Academia academia;
         private JTextField nomeAlunoField;
         private JTextField idadeAlunoField;
@@ -18,9 +18,26 @@ import java.awt.event.ActionListener;
         academia = new Academia();
 
         setTitle("Gestão de Academia");
-        setSize(600,400);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        JPanel painelAluno = new JPanel(new GridLayout(4,2));
+        painelAluno.add(new JLabel("Nome: "));
+        nomeAlunoField = new JTextField();
+        painelAluno.add(nomeAlunoField);
+        painelAluno.add(new JLabel("Idade: "));
+        idadeAlunoField = new JTextField();
+        painelAluno.add(idadeAlunoField);
+        painelAluno.add(new JLabel("Aparelho: "));
+        aparelhosAlunoField = new JTextField();
+        painelAluno.add(aparelhosAlunoField);
+        JButton adicionarAlunoBtn = new JButton("Adicionar Aluno");
+        painelAluno.add(adicionarAlunoBtn);
+        JButton removerAlunoBtn = new JButton("Remover Aluno");
+        painelAluno.add(removerAlunoBtn);
+
+
     }
 }
 
