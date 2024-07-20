e291320f4a0b004b661be26352ff069f857b41ee
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-20 09:41:51 -0300

    Feat: Criar váriaveis para a interface e uma classe para iniciar painel e definições

--- src/AcademiaGUI.java
+++ src/AcademiaGUI.java
@@ -6,6 +6,21 @@ import java.awt.event.ActionListener;
     public class AcademiaGUI {
         private Academia academia;
         private JTextField nomeAlunoField;
+        private JTextField idadeAlunoField;
+        private JTextField aparelhosAlunoField;
+        private JTextField alunosArea;
+        private JTextField nomeApartamentoField;
+        private JTextField descricaoAparelhoField;
+        private JTextArea aparelhoArea;
 
 
+    public AcademiaGUI() {
+        academia = new Academia();
+
+        setTitle("Gestão de Academia");
+        setSize(600,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+    }
 }
+
