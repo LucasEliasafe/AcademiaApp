d23c5f3b945a38456e60d988729ee06d557bd1d5
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-20 09:25:42 -0300

    Feat: Criar classe para colocar interface gráfica com Jframe

---
+++ src/AcademiaGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+    public class AcademiaGUI {
+        private Academia academia;
+        private JTextField nomeAlunoField;
+
+
+}
